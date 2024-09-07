49bcfeef6a03ca40aa325c517a07040c45ba910f
nbesoro <ibrasoro04@gmail.com>
2024-09-07 23:36:59 +0000

    refactor: add field to Customer model

--- account/models/customer.py
+++ account/models/customer.py
@@ -5,6 +5,9 @@ from .user import User
 
 class Customer(User):
     phone = models.CharField(max_length=constants.VERY_SHORT_CHARFIELD)
+    phone2 = models.CharField(
+        max_length=constants.VERY_SHORT_CHARFIELD, null=True, blank=True
+    )
     addresse = models.CharField(max_length=constants.NORMAL_CHARFIELD)
     postal_code = models.CharField(max_length=constants.VERY_SHORT_CHARFIELD)
     city = models.CharField(max_length=constants.SHORT_CHARFIELD)
